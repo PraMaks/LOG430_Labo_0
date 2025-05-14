0f75341901a244b2821a95ae87b82c8656d3ef27
PraMaks <maks111111112@gmail.com>
2025-05-14 14:52:27 +0000

    Fix de Pylint pour test_pytest

--- test_pytest.py
+++ test_pytest.py
@@ -1,8 +1,11 @@
 def addition(x, y):
+    """Function qui fait l'addition de 2 nombres"""
     return x + y
 
 def test_addition_positive():
+    """Test d'addition de 2 nombres positives"""
     assert addition(4, 2) == 6
 
 def test_addition_negative():
+    """Test d'addition de 2 nombres negatives"""
     assert addition(-4, -2) == -6
