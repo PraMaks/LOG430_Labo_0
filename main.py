89d4317a0e8c12124b13764ae5deb9a0ad75b837
PraMaks <maks111111112@gmail.com>
2025-05-14 14:50:08 +0000

    Fix pour le Pylint pour main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+"""Import de la fonction d'addition"""
 from test_pytest import addition
 
 def main():
+    """Function main qui lance la fonction d'addition"""
     print(addition(4,5))
 
-# Using the special variable 
+# Using the special variable
 # __name__
 if __name__=="__main__":
     main()
