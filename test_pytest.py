821d1e7d0560818fdd6567e59a81fb76ff2393c0
PraMaks <maks111111112@gmail.com>
2025-05-23 16:35:40 +0000

    Ajout des premiers tests unitaires

--- test_pytest.py
+++ test_pytest.py
@@ -1,10 +1,27 @@
-"""Fichier avec 2 tests unitaires"""
-from main import addition
+"""Module principal des tests unitaires de l'application."""
+import pytest
+from mongoengine import connect, disconnect
+from main import StoreInventory, StoreSale, search_product
 
-def test_addition_positive():
-    """Test d'addition de 2 nombres positives"""
-    assert addition(4, 2) == 6
+TEST_DB_NAME = "test_labo1"
 
-def test_addition_negative():
-    """Test d'addition de 2 nombres negatives"""
-    assert addition(-4, -2) == -6
+@pytest.fixture(scope="function", autouse=True)
+def setup_and_teardown_db():
+    """Fonction lancée avant chaque test pour raser la bd de test"""
+    disconnect()  # Déconnecte toute connexion existante avant d'en créer une nouvelle
+    connect(TEST_DB_NAME, host="localhost", port=27017, alias="default")
+    StoreInventory.drop_collection()
+    StoreSale.drop_collection()
+    yield
+    StoreInventory.drop_collection()
+    StoreSale.drop_collection()
+    disconnect()
+
+def test_search_product_found():
+    """Test pour chercher un produit"""
+    StoreInventory(name="TestProduct", price=10, qty=5).save()
+    product = search_product("TestProduct")
+    assert product is not None
+    assert product["name"] == "TestProduct"
+    assert product["price"] == 10
+    assert product["qty"] == 5
