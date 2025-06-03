0690bbc58a23a3b0e7794e8efdf68dd79c660bc1
PraMaks <maks111111112@gmail.com>
2025-06-03 20:24:14 +0000

    Ajustements au fichier SupplyRequest

--- src/backend/models/SupplyRequest.js
+++ src/backend/models/SupplyRequest.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
 
+const productRequestedSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    qty: { type: Number, required: true }
+}, { _id: false });
+
 const supplyRequestSchema = new mongoose.Schema({
     store: { type: mongoose.Schema.Types.ObjectId, ref: 'Store', required: true },
-    products: [
-        {
-            name: { type: String, required: true },
-            qty: { type: Number, required: true }
-        }
-    ],
-    status: { type: String, enum: ['pending', 'approved', 'rejected', 'fulfilled'], default: 'pending' },
-    requestDate: { type: Date, default: Date.now },
+    products: { type: [productRequestedSchema], required: true },
+    status: { 
+        type: String, 
+        enum: ['pending', 'approved', 'rejected', 'fulfilled'], 
+        default: 'pending' 
+    },
+    requestDate: { type: Date, default: () => new Date() }
 }, {
     collection: 'demandesApprovisionnement'
 });
