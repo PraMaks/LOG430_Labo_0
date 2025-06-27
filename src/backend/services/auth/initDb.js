154863d8e044e97f43e8fdf8fed0d7b96b4a1d43
Pravdin Maksym <maks111111112@gmail.com>
2025-06-27 15:12:11 -0400

    Cleanup pour Auth

--- src/backend/services/auth/initDb.js
+++ src/backend/services/auth/initDb.js
@@ -1,5 +1,4 @@
 const Store = require('./models/Store');
-const StoreInventory = require('./models/StoreInventory');
 const User = require('./models/User');
 const logger = require('./utils/logger');
 
@@ -11,12 +10,6 @@ const DEFAULT_STORES = [
   { name: 'Magasin 5', address: '654 route Est', nb_requests: 0, is_store: true },
   { name: 'Magasin Central', address: '146 route Centrale', nb_requests: 0, is_store: true },
   { name: 'Stock Central', address: '257 route Centrale', nb_requests: 0, is_store: false }
-];
-
-const DEFAULT_PRODUCTS = [
-  { name: 'Bread', description: 'Very Tasty Bread', price: 4, qty: 5, max_qty: 5 },
-  { name: 'Soda', description: 'Very Refreshing', price: 3, qty: 10, max_qty: 10 },
-  { name: 'Candy', description: 'Very Sugary', price: 2, qty: 15, max_qty: 15 }
 ];
 
 const DEFAULT_USERS = [
@@ -53,23 +46,6 @@ async function initDb() {
       continue;
     }
 
-    for (const product of DEFAULT_PRODUCTS) {
-      const productName = product.name + counter;
-      const exists = await StoreInventory.findOne({ store: store._id, name: productName });
-      if (!exists) {
-        const inventoryItem = new StoreInventory({
-          store: store._id,
-          name: productName,
-          description: product.description,
-          price: product.price,
-          qty: product.qty,
-          max_qty: product.max_qty
-        });
-        await inventoryItem.save();
-        logger.info(`Ajout de '${productName}' au magasin '${store.name}'`);
-      }
-    }
-
     counter++;
   }
 
@@ -101,27 +77,6 @@ async function initDb() {
       logger.info(`Utilisateur '${user.username}' créé.`);
     } else {
       logger.info(`Utilisateur '${user.username}' déjà existant.`);
-    }
-  }
-
-  // Copie des produits dans le stock central
-  if (centralStock) {
-    const allOtherProducts = await StoreInventory.find({ store: { $ne: centralStock._id } });
-
-    for (const product of allOtherProducts) {
-      const alreadyExists = await StoreInventory.findOne({ store: centralStock._id, name: product.name });
-      if (!alreadyExists) {
-        const copiedProduct = new StoreInventory({
-          store: centralStock._id,
-          name: product.name,
-          description: product.description,
-          price: product.price,
-          qty: product.qty * 10,
-          max_qty: product.max_qty * 10
-        });
-        await copiedProduct.save();
-        logger.info(`Ajout de '${product.name}' dans 'Stock Central'`);
-      }
     }
   }
 
